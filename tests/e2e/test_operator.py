7e991ace5ad3c3aa3a4d0e54c55e9607c3e72d04
Michael Arndt <arndt@menedev.de>
2024-12-02 17:15:11 +0100

    Fix

--- tests/e2e/test_operator.py
+++ tests/e2e/test_operator.py
@@ -737,7 +737,7 @@ def test_011_1(self):
                 "select 'OK'",
                 host="chi-test-011-secured-cluster-default-1-0",
                 user="user1",
-                pwd="l%XubpKqz2y!QsKlsynEEE6#Thknj&fG",
+                pwd="topsecret",
             )
             assert out == "OK"
 
@@ -1906,20 +1906,6 @@ def test_016(self):
         start_time = kubectl.get_field("pod", f"chi-{chi}-default-0-0-0", ".status.startTime")
         kubectl.create_and_check(
             manifest="manifests/chi/test-016-settings-06.yaml",
-            check={
-                "do_not_delete": 1,
-            },
-        )
-
-        with And("ClickHouse SHOULD NOT BE restarted"):
-            new_start_time = kubectl.get_field("pod", f"chi-{chi}-default-0-0-0", ".status.startTime")
-            assert start_time == new_start_time
-
-    # test-016-settings-07.yaml
-    with When("Add I change a number of settings that does not require a restart"):
-        start_time = kubectl.get_field("pod", f"chi-{chi}-default-0-0-0", ".status.startTime")
-        kubectl.create_and_check(
-            manifest="manifests/chi/test-016-settings-07.yaml",
             check={
                 "do_not_delete": 1,
             },
